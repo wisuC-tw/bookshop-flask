97fcbce1f703358f346e718f53ad396e9651ef12
Choi Wi Su <wisuchoi@Chois-MacBook-Pro.local>
2021-07-13 14:06:46 +0800

    Add api routes to get books sorted by price
    Add route /books-sorted-ascending which returns a list of books in ascending price.
    Add route /books-sorted-descending which returns a list of books in descending price.

--- project/__init__.py
+++ project/__init__.py
@@ -28,15 +28,16 @@ def setup_routes(app):
 
     @app.route('/books')
     def books():
-        db = get_db()
-        rows = db.execute(
-            "SELECT * FROM books50"
-        ).fetchall()
+        return run_query("SELECT * FROM books50")
 
-        column_names = rows[0].keys()
-        data = [dict(zip(column_names, row)) for row in rows]
-        
-        return jsonify(data)
+    @app.route('/books-sorted-ascending')
+    def books_sorted_ascending():
+        return run_query("SELECT * FROM books50 ORDER BY price")
+
+    @app.route('/books-sorted-descending')
+    def books_sorted_descending():
+        return run_query("SELECT * FROM books50 ORDER BY price DESC")
+
 
 def setup_logging(app):
     formatter = logging.Formatter(
@@ -51,3 +52,14 @@ def setup_logging(app):
     def after_request(response):
         app.logger.info('%s %s %s %s %s', request.remote_addr, request.method, request.scheme, request.full_path, response.status)
         return response
+
+def run_query(query_string):
+        db = get_db()
+        rows = db.execute(
+            query_string
+        ).fetchall()
+
+        column_names = rows[0].keys()
+        data = [dict(zip(column_names, row)) for row in rows]
+        
+        return jsonify(data)

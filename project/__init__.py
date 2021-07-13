1a16e70478283cfcd01118e15e7f277ae0aa4b75
Choi Wi Su <wisuchoi@Chois-MacBook-Pro.local>
2021-07-13 13:36:53 +0800

    Add logging incoming requests to file
    Add a logging handler on DEBUG level to write logs to a file named debug.log
    In the format [datetime]{file:linenumber} loglevel - message

--- project/__init__.py
+++ project/__init__.py
@@ -1,5 +1,34 @@
-from flask import Flask
+from flask import Flask, request, jsonify, g
+import logging
+from logging.handlers import WatchedFileHandler
+import os 
 
 def create_app(config_filename=None):
     app= Flask(__name__)
+    setup_routes(app)
+    setup_logging(app)
+
     return app
+
+def setup_routes(app):
+    @app.route('/')
+    def index():
+        return 'Index Page'
+
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World'
+
+def setup_logging(app):
+    formatter = logging.Formatter(
+        "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+    handler = WatchedFileHandler('debug.log')
+    handler.setLevel(logging.DEBUG)
+    handler.setFormatter(formatter)
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.DEBUG)
+
+    @app.after_request
+    def after_request(response):
+        app.logger.info('%s %s %s %s %s', request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+        return response

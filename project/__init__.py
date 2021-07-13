c637425c27e19278175cc5bbe0d5c1f4e32fed96
Choi Wi Su <wisuchoi@Chois-MacBook-Pro.local>
2021-07-13 13:40:32 +0800

    Add db connection setup and api route /books
    Add db connection setup in create_app.
    Add api route /books, which queries the db for all books and fields,
    and returns the data in .json format.

--- project/__init__.py
+++ project/__init__.py
@@ -2,11 +2,18 @@ from flask import Flask, request, jsonify, g
 import logging
 from logging.handlers import WatchedFileHandler
 import os 
+from project.db import get_db
 
 def create_app(config_filename=None):
     app= Flask(__name__)
+    
+    app.config['DATABASE_URI']=os.path.join(app.root_path, "booklist.db")
+
     setup_routes(app)
     setup_logging(app)
+
+    from project import db
+    db.init_app(app)
 
     return app
 
@@ -18,6 +25,18 @@ def setup_routes(app):
     @app.route('/hello')
     def hello():
         return 'Hello, World'
+
+    @app.route('/books')
+    def books():
+        db = get_db()
+        rows = db.execute(
+            "SELECT * FROM books50"
+        ).fetchall()
+
+        column_names = rows[0].keys()
+        data = [dict(zip(column_names, row)) for row in rows]
+        
+        return jsonify(data)
 
 def setup_logging(app):
     formatter = logging.Formatter(
